3d603814509b148fffc6275e71522a2cff7fbac4
Zein Ersyad <zeinersyad@gmail.com>
2026-06-04 15:58:46 +0700

    feat(changelog): add toast component with custom options

--- plugins/toast.ts
+++ plugins/toast.ts
@@ -2,6 +2,6 @@ import Vue3Toastify, {toast} from "vue3-toastify";
 import "vue3-toastify/dist/index.css";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(Vue3Toastify, { autoClose: 3000, clearOnUrlChange: false })
+  nuxtApp.vueApp.use(Vue3Toastify, { autoClose: 3000, clearOnUrlChange: false, multiple: false })
   return { provide: { toast }}
 })
